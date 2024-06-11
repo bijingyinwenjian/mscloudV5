d0d2eadff2ae1f9d9a7d9ad35a22d3c613d552e2
yinjiang <yinjiang@exworth.group>
2024-06-11 17:35:07 +0800

    1

--- cloud-api-commons/src/main/java/com/atguigu/cloud/apis/PayFeignApi.java
+++ cloud-api-commons/src/main/java/com/atguigu/cloud/apis/PayFeignApi.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient("cloud-payment-service")
+@FeignClient("com.atguigu.cloud-payment-service")
 public interface PayFeignApi {
     /**
      * 新增一条支付相关流水记录
